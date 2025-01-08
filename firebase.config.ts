d0118523c3bd693abbd32f1eef996e9e515c4307
Veronika Bacova <vachal.vojta@email.cz>
2025-01-08 18:31:17 +0100

    Checking if pushing possible

--- firebase.config.ts
+++ firebase.config.ts
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   // Your firebase config - add to firebase project new WEB project and copy config here.
-  // IMPORTANT - add this to .gitignore file
+  // IMPORTANT - add this to .gitignore fileasdf
 };
 
 const app = initializeApp(firebaseConfig);
